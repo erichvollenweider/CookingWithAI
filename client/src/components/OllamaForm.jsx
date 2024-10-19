98135c86e592f39dde49e4be36042917fa4d7c3e
erichvollenweider <vollenweidererich@yahoo.com.ar>
2024-10-19 11:06:42 -0300

    OllaForm CSS refactoring

--- client/src/components/OllamaForm.jsx
+++ client/src/components/OllamaForm.jsx
@@ -78,7 +78,7 @@ const OllamaForm = ({ onLogout }) => {
         consejos.push(line.trim());
       } else if (!title) {
         title = line.replace("##", "").trim();  // Eliminamos el "##" del título
-      
+
         if (title.includes("Receta generada para los ingredientes:")) {
           title = title.replace("Receta generada para los ingredientes:", "").trim();
         }
@@ -138,49 +138,99 @@ const OllamaForm = ({ onLogout }) => {
         <div className={styles.bottomButton}>
           <button onClick={onLogout} className={styles.logoutButton}>
             Cerrar sesión
-          </button> 
+          </button>
         </div>
       </div>
       <div className={styles.chatContainer}>
-        <img src="../../public/icon.png"></img>
         <h1>CookingWithAI</h1>
-        <h2>Sube una imagen o ingresa los ingredientes para obtener una receta</h2>
+        <h3>Descubre recetas con ingredientes en casa</h3>
+        <img src="../../public/icon.png"></img>
 
         <div className={styles.submits}>
           <form onSubmit={handleSubmit}>
-            <div className={styles.x}>
-              <label className={styles.x}>Subir imágenes:</label>
-              <input
-                type="file"
-                accept="image/*"
-                multiple
-                onChange={(e) => {
-                  setFiles(Array.from(e.target.files));
-                }}
-                className={styles.uploadButton}
-              />
-            </div>
 
-            <div className={styles.x}>
-              <label className={styles.x}>Ingresa ingredientes:</label>
+            <div className={styles.messageBox}>
+              <div className={styles.fileUploadWrapper}>
+                <label htmlFor="file">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 337 337">
+                    <circle
+                      stroke-width="20"
+                      stroke="#6c6c6c"
+                      fill="none"
+                      r="158.5"
+                      cy="168.5"
+                      cx="168.5"
+                    ></circle>
+                    <path
+                      stroke-linecap="round"
+                      stroke-width="25"
+                      stroke="#6c6c6c"
+                      d="M167.759 79V259"
+                    ></path>
+                    <path
+                      stroke-linecap="round"
+                      stroke-width="25"
+                      stroke="#6c6c6c"
+                      d="M79 167.138H259"
+                    ></path>
+                  </svg>
+                  <span
+                    className={styles.tooltip}>Add an image
+                  </span>
+                </label>
+                <input
+                  type="file"
+                  id="file"
+                  accept="image/*"
+                  multiple
+                  onChange={(e) => {
+                    setFiles(Array.from(e.target.files));
+                  }}
+                  className={styles.file}
+                  name="file"
+                />
+              </div>
               <input
+                placeholder="Ingredientes..."
                 type="text"
                 value={text}
                 onChange={(e) => {
                   setText(e.target.value);
                 }}
-                className={styles.uploadText}
+                className={styles.messageInput}
               />
+              <button className={styles.sendButton} type="submit">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 664 663">
+                  <path
+                    fill="none"
+                    d="M646.293 331.888L17.7538 17.6187L155.245 331.888M646.293 331.888L17.753 646.157L155.245 331.888M646.293 331.888L318.735 330.228L155.245 331.888"
+                  ></path>
+                  <path
+                    stroke-linejoin="round"
+                    stroke-linecap="round"
+                    stroke-width="33.67"
+                    stroke="#6c6c6c"
+                    d="M646.293 331.888L17.7538 17.6187L155.245 331.888M646.293 331.888L17.753 646.157L155.245 331.888M646.293 331.888L318.735 330.228L155.245 331.888"
+                  ></path>
+                </svg>
+              </button>
             </div>
-            <button type="submit" className={styles.sendButton}>
-              Enviar
-            </button>
           </form>
         </div>
 
         <div className={styles.aiResponse}>
-          <h1>Zona de respuestas (Quitar texto luego)</h1>
-          {loading && <p>Cargando...</p>}
+          {loading &&
+            (<div className={styles.spinner}>
+              <span>C</span>
+              <span>O</span>
+              <span>C</span>
+              <span>I</span>
+              <span>N</span>
+              <span>A</span>
+              <span>N</span>
+              <span>D</span>
+              <span>O</span>
+            </div>)}
           {response && renderRecipe()}
           {error && <p className={styles.errorMessage}>Error: {error}</p>}
         </div>
