aa4fbdaff42bae1a9cf6f95293b7c5b5ca7e1bc1
Bruno Conti <128512240+brunocontii@users.noreply.github.com>
2024-10-31 13:39:25 -0300

    logic for the news models of dataset

--- server/app.py
+++ server/app.py
@@ -17,18 +17,38 @@ bcrypt = Bcrypt(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Construir la ruta absoluta para el archivo CSV
-csv_frutas_verduras = os.path.join(basedir, 'dataset_frutas_verduras.csv')
+csv_v1 = os.path.join(basedir, 'dataset-v1.csv')
+csv_v2 = os.path.join(basedir, 'dataset-v2.csv')
+csv_f = os.path.join(basedir, 'dataset-f.csv')
 csv_file_carnes = os.path.join(basedir, 'dataset-carnes.csv')
 
 # Verificar si el archivo CSV existe
-if os.path.exists(csv_frutas_verduras):
-    df_frutas_verduras = pd.read_csv(csv_frutas_verduras)
-    class_labels_frutas_verduras = df_frutas_verduras.columns[1:]  # Etiquetas para frutas y verduras
+if os.path.exists(csv_v1):
+    df_v1 = pd.read_csv(csv_v1)
+    class_labels_v1 = df_v1.columns[1:]  # Etiquetas para frutas y verduras
     print("CSV frutas y verduras cargado correctamente.")
 else:
-    print(f"El archivo CSV frutas y verduras no se encuentra en la ruta: {csv_frutas_verduras}")
+    print(f"El archivo CSV frutas y verduras no se encuentra en la ruta: {csv_v1}")
     # Manejamos el error con una excepción
-    raise FileNotFoundError(f"El archivo CSV frutas y verduras no se encuentra en la ruta: {csv_frutas_verduras}")
+    raise FileNotFoundError(f"El archivo CSV frutas y verduras no se encuentra en la ruta: {csv_v1}")
+
+if os.path.exists(csv_v2):
+    df_v2 = pd.read_csv(csv_v2)
+    class_labels_v2 = df_v2.columns[1:]  # Etiquetas para frutas y verduras
+    print("CSV frutas y verduras cargado correctamente.")
+else:
+    print(f"El archivo CSV frutas y verduras no se encuentra en la ruta: {csv_v2}")
+    # Manejamos el error con una excepción
+    raise FileNotFoundError(f"El archivo CSV frutas y verduras no se encuentra en la ruta: {csv_v2}")
+
+if os.path.exists(csv_f):
+    df_f = pd.read_csv(csv_f)
+    class_labels_f = df_f.columns[1:]  # Etiquetas para frutas y verduras
+    print("CSV frutas y verduras cargado correctamente.")
+else:
+    print(f"El archivo CSV frutas y verduras no se encuentra en la ruta: {csv_f}")
+    # Manejamos el error con una excepción
+    raise FileNotFoundError(f"El archivo CSV frutas y verduras no se encuentra en la ruta: {csv_f}")
 
 if os.path.exists(csv_file_carnes):
     df_carnes = pd.read_csv(csv_file_carnes)
@@ -38,18 +58,40 @@ else:
     print(f"El archivo CSV carnes no se encuentra en la ruta: {csv_file_carnes}")
     raise FileNotFoundError(f"El archivo CSV carnes no se encuentra en la ruta: {csv_file_carnes}")
 
-model_frutas_verduras_path = os.path.join(basedir, 'modeloEntrenado')
+model_v1_path = os.path.join(basedir, 'modeloEntrenado-v1')
+model_v2_path = os.path.join(basedir, 'modeloEntrenado-v2')
+model_f_path = os.path.join(basedir, 'modeloEntrenado-f')
 model_carnes_path = os.path.join(basedir, 'modeloEntrenadoCarnes')
 
-if os.path.exists(model_frutas_verduras_path):
-    print(f'La ruta {model_frutas_verduras_path} es válida y existe.')
+if os.path.exists(model_v1_path):
+    print(f'La ruta {model_v1_path} es válida y existe.')
     # Cargar el modelo
-    model_frutas_verduras = tf.keras.models.load_model(model_frutas_verduras_path)
+    model_v1 = tf.keras.models.load_model(model_v1_path)
     print("Modelo frutas y verduras cargado correctamente.")
 else:
-    print(f'La ruta {model_frutas_verduras_path} no existe o es incorrecta.')
+    print(f'La ruta {model_v1_path} no existe o es incorrecta.')
     # Manejamos el error con una excepción
-    raise FileNotFoundError(f'La ruta {model_frutas_verduras_path} no existe o es incorrecta.')
+    raise FileNotFoundError(f'La ruta {model_v1_path} no existe o es incorrecta.')
+
+if os.path.exists(model_v2_path):
+    print(f'La ruta {model_v2_path} es válida y existe.')
+    # Cargar el modelo
+    model_v2 = tf.keras.models.load_model(model_v2_path)
+    print("Modelo frutas y verduras cargado correctamente.")
+else:
+    print(f'La ruta {model_v2_path} no existe o es incorrecta.')
+    # Manejamos el error con una excepción
+    raise FileNotFoundError(f'La ruta {model_v2_path} no existe o es incorrecta.')
+
+if os.path.exists(model_f_path):
+    print(f'La ruta {model_f_path} es válida y existe.')
+    # Cargar el modelo
+    model_f = tf.keras.models.load_model(model_f_path)
+    print("Modelo frutas y verduras cargado correctamente.")
+else:
+    print(f'La ruta {model_f_path} no existe o es incorrecta.')
+    # Manejamos el error con una excepción
+    raise FileNotFoundError(f'La ruta {model_f_path} no existe o es incorrecta.')
 
 if os.path.exists(model_carnes_path):
     print(f'La ruta {model_carnes_path} es válida y existe.')
@@ -85,22 +127,65 @@ def preprocess_image(image):
 
 def get_ingredients_from_image(image):
     img_array = preprocess_image(image)
-    predictions_frutas_verduras = model_frutas_verduras.predict(img_array)
+    predictions_v1 = model_v1.predict(img_array)
+    predictions_v2 = model_v2.predict(img_array)
+    predictions_f = model_f.predict(img_array)
     predictions_carnes = model_carnes.predict(img_array)
     
     # Obtener las etiquetas predichas usando un umbral
-    predicted_labels_frutas_verduras = (predictions_frutas_verduras > 0.5).astype(int)
-    predicted_labels_carnes = (predictions_carnes > 0.8).astype(int)
+    predicted_labels_v1 = (predictions_v1 > 0.3).astype(int)
+    predicted_labels_v2 = (predictions_v2 > 0.5).astype(int)
+    predicted_labels_f = (predictions_f > 0.5).astype(int)
+    predicted_labels_carnes = (predictions_carnes > 0.82).astype(int)
 
-    print(f"Predicciones frutas_verduras: {predictions_frutas_verduras}")
-    print(f"Etiquetas predichas frutas_verduras: {predicted_labels_frutas_verduras}")
-    
-    print(f"Predicciones carnes: {predictions_carnes}")
-    print(f"Etiquetas predichas cares: {predicted_labels_carnes}")
+    # Imprimir predicciones en una línea continua
+    print("Predicciones verduras V1: ")
+    labels_v1 = ["anquito", "apio", "berenjena", "cebolla", "cebolla morada", "choclo", "coliflor", 
+                "huevo", "lechuga", "papa", "pimiento amarillo", "pimiento rojo", "pimiento verde", "remolacha"]
+
+    # Convertir predicciones a una lista de texto
+    predictions_text_v1 = [f"{label}: {pred:.2f}" for label, pred in zip(labels_v1, predictions_v1[0])]
+    print(", ".join(predictions_text_v1))
+
+    # Imprimir etiquetas predichas en una línea continua
+    predicted_labels_text_v1 = [label for label, pred in zip(labels_v1, predicted_labels_v1[0]) if pred == 1]
+    print("Etiquetas predichas verduras V1:", ", ".join(predicted_labels_text_v1))
+    print("-----------------------------------------------------------------------------------------------------")
+
+    print("Predicciones verduras V2: ")
+    labels_v2 = ["ajo","arveja", "batata", "brocoli", "cebolla de verdeo", "espinaca", "palta", "pepino","rabanito", "repollo morado",
+                 "tomate","zanahoria", "zapallito"]
+
+    predictions_text_v2 = [f"{label}: {pred:.2f}" for label, pred in zip(labels_v2, predictions_v2[0])]
+    print(", ".join(predictions_text_v2))
+
+    predicted_labels_text_v2 = [label for label, pred in zip(labels_v2, predicted_labels_v2[0]) if pred == 1]
+    print("Etiquetas predichas verduras V2:", ", ".join(predicted_labels_text_v2))
+    print("-----------------------------------------------------------------------------------------------------")
+
+    print("Predicciones frutas: ")
+    labels_f = ["aceituna", "ananá", "banana", "cereza", "durazno", "frutilla", "jengibre", "kiwi", "limón", "manzana", "naranja", "pera", "sandía"]
+
+    predictions_text_f = [f"{label}: {pred:.2f}" for label, pred in zip(labels_f, predictions_f[0])]
+    print(", ".join(predictions_text_f))
+
+    predicted_labels_text_f = [label for label, pred in zip(labels_f, predicted_labels_f[0]) if pred == 1]
+    print("Etiquetas predichas frutas:", ", ".join(predicted_labels_text_f))
+    print("-----------------------------------------------------------------------------------------------------")
+
+    print("Predicciones carnes: ")
+    labels_c = ["alita", "chinchulin", "chorizo", "costeleta de cerdo", "hamburguesa", "milanesa", "morcilla", "pan", "pata-muslo", "pechuga", "pollo", "riñon"]
+
+    predictions_text_c = [f"{label}: {pred:.2f}" for label, pred in zip(labels_c, predictions_carnes[0])]
+    print(", ".join(predictions_text_c))
+
+    predicted_labels_text_c = [label for label, pred in zip(labels_c, predicted_labels_carnes[0]) if pred == 1]
+    print("Etiquetas predichas frutas:", ", ".join(predicted_labels_text_c))
+    print("-----------------------------------------------------------------------------------------------------")
 
     # Concatenar etiquetas y predicciones en una sola lista para simplificar
-    all_labels = list(class_labels_frutas_verduras) + list(class_labels_carnes)
-    all_predictions = np.concatenate([predicted_labels_frutas_verduras[0], predicted_labels_carnes[0]])
+    all_labels = list(class_labels_v1) + list(class_labels_v2) + list(class_labels_f) + list(class_labels_carnes)
+    all_predictions = np.concatenate([predicted_labels_v1[0], predicted_labels_v2[0], predicted_labels_f[0], predicted_labels_carnes[0]])
 
     # Obtener ingredientes en una sola línea
     ingredients = [all_labels[i] for i in range(len(all_predictions)) if all_predictions[i] == 1]
