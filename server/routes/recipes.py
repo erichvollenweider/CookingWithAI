5d8cd677adf45851759f1e168db4d4ea06b93a65
Bruno Conti <brunocse.5@gmail.com>
2024-11-15 18:34:18 -0300

    added more recipes and a method for get recipes

--- server/routes/recipes.py
+++ server/routes/recipes.py
@@ -35,3 +35,29 @@ def guardar_receta():
     except Exception as e:
         print(f"Error al guardar receta: {str(e)}")  # Imprime el error en la consola del servidor
         return jsonify({'error': str(e)}), 500
+
+@recipes_bp.route('/get_recipes', methods=['GET'])
+def get_recipes():
+    if 'logged_in' not in session or not session['logged_in']:
+        return jsonify({'error': 'No hay usuario logeado'}), 401
+
+    user_id = session.get('user_id')
+    user = Users.query.get(user_id)
+
+    if not user:
+        return jsonify({'error': 'Usuario no encontrado'}), 404
+
+    recetas = Recetas.query.filter_by(user_id=user_id).all()
+
+    if not recetas:
+        return jsonify({'error': 'El usuario no tiene recetas guardadas'}), 404
+
+    recetas_dic = [
+        {
+            'id': recetas.id,
+            'titulo': recetas.titulo,
+            'descripcion': recetas.descripcion,
+        } for recetas in recetas
+    ]
+
+    return jsonify({'message': 'Recetas obtenidas correctamente', 'recetas': recetas_dic}), 200
